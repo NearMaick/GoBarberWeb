43625f22fdc561a4b1b9d51581c719542261d0d0
Maick Souza <maick_a_s@msn.com>
2020-04-01 01:29:02 -0300

    feat(web): socket.io and avatar adjustments

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,9 @@
+import 'dotenv/config';
+
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3333',
+  baseURL: `http://localhost:3333`,
 });
 
 export default api;
